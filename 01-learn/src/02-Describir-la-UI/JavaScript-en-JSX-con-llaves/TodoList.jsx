6a3799ab67d37d7891f70b16d63a24182e3a48ce
Dixon G <dxxn0101@gmail.com>
2023-01-26 11:51:54 -0600

    Más diversión con objetos de JavaScript y llaves

--- 01-learn/src/02-Describir-la-UI/JavaScript-en-JSX-con-llaves/TodoList.jsx
+++ 01-learn/src/02-Describir-la-UI/JavaScript-en-JSX-con-llaves/TodoList.jsx
@@ -2,17 +2,20 @@ import React from "react";
 
 const hoy = new Date();
 
+const persona = {
+  nombre: "Gregorio Y. Zara",
+  temaOscuro: { backgroundColor: "black", color: "pink" },
+};
+
+const { nombre, temaOscuro } = persona;
+
 export const TodoList = () => {
-  const nombre = "Gregorio Y. Zara";
   return (
     <>
       <h1>Lista de tareas de {nombre}</h1>
       <h2 className="h1">Lista de tareas del {formatearFecha(hoy)}</h2>
 
-      <ul
-        style={{ backgroundColor: "black", color: "pink" }}
-        className="p-3 rounded"
-      >
+      <ul style={temaOscuro} className="p-3 rounded">
         <li>Mejorar el videoteléfono</li>
         <li>Preparar las clases de aeronáutica</li>
         <li>Trabajar en el motor de alcohol</li>
