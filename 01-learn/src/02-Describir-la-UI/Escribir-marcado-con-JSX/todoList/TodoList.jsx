39aaa83e22080e68e054b9e0f0867e1af5f775e2
Dixon G <dxxn0101@gmail.com>
2023-01-26 09:28:33 -0600

    Las Reglas de JSX

--- 01-learn/src/02-Describir-la-UI/Escribir-marcado-con-JSX/todoList/TodoList.jsx
+++ 01-learn/src/02-Describir-la-UI/Escribir-marcado-con-JSX/todoList/TodoList.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 export const TodoList = () => {
   return (
-
+    <>
     <h1>Hedy Lamarr's Todos</h1>
 <img 
   src="https://i.imgur.com/yXOvdOSs.jpg" 
   alt="Hedy Lamarr" 
-  class="photo"
->
+  className="photo"
+/>
 <ul>
-    <li>Invent new traffic lights
-    <li>Rehearse a movie scene
-    <li>Improve the spectrum technology
+    <li>Invent new traffic lights</li>
+    <li>Rehearse a movie scene</li>
+    <li>Improve the spectrum technology</li>
 </ul>
-
+    </>
   )
 }
