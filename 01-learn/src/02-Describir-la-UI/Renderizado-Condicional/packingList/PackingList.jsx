f8ef2badf0dc5dc3055a9f33f9cbc888cdc90e70
Dixon G <dxxn0101@gmail.com>
2023-01-27 11:00:07 -0600

    Exclusión condicional de JSX

--- 01-learn/src/02-Describir-la-UI/Renderizado-Condicional/packingList/PackingList.jsx
+++ 01-learn/src/02-Describir-la-UI/Renderizado-Condicional/packingList/PackingList.jsx
@@ -9,14 +9,27 @@ export const PackingList = () => {
         <Item empaquetado={false} nombre="Foto de Tam" />
         <Item empaquetado={true} nombre="Casco con hoja dorada" />
       </ul>
+
+      <h1>Lista de equipaje de Sally Ride 02</h1>
+      <ul>
+        <Item2 empaquetado={true} nombre="Mochila impermeable" />
+        <Item2 empaquetado={true} nombre="Toalla de microfibra" />
+        <Item2 empaquetado={false} nombre="Botiquín básico" />
+      </ul>
     </section>
   );
 };
 
 function Item({ empaquetado, nombre }) {
-  if (empaquetado) {
-    return <li className="item">{nombre} ✔</li>;
-  }
+  return (
+    <li className="item">{empaquetado ? <del>{nombre + "✔"}</del> : nombre}</li>
+  );
+}
 
-  return <li className="item">{nombre}</li>;
+function Item2({ empaquetado, nombre }) {
+  return (
+    <li className="item">
+      {nombre} {empaquetado && "✔"}
+    </li>
+  );
 }
