9d82aa5201cab12d3b143da63edc095f3f91cf98
Dixon G <dxxn0101@gmail.com>
2023-01-28 09:15:27 -0600

    Filtrar arrays de objetos

--- 01-learn/src/02-Describir-la-UI/Renderizado-de-listas/list/List.jsx
+++ 01-learn/src/02-Describir-la-UI/Renderizado-de-listas/list/List.jsx
@@ -1,3 +1,5 @@
+import { getimageUrl } from "../../overview/profile/utils";
+
 const personas = [
   "Creola Katherine Johnson: mathematician",
   "Mario José Molina-Pasquel Henríquez: chemist",
@@ -9,11 +11,68 @@ const personas = [
   },
 ];
 
+const personas2 = [
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+    accomplishment: "spaceflight calculations",
+    imageId: "MK3eW3A",
+  },
+  {
+    id: 1,
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+    accomplishment: "discovery of Arctic ozone hole",
+    imageId: "mynHUSa",
+  },
+  {
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+    accomplishment: "electromagnetism theory",
+    imageId: "bE7W1ji",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+    accomplishment:
+      "pioneering cortisone drugs, steroids and birth control pills",
+    imageId: "IOjWm71",
+  },
+  {
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+    accomplishment: "white dwarf star mass calculations",
+    imageId: "lrWQx8l",
+  },
+];
+
 export const List = () => {
   const listaElementos = personas.map((persona) => <li>{persona}</li>);
+
+  const chemist = personas2.filter(
+    (persona) => persona.profession === "chemist"
+  );
+
+  const chemistListaElementos = chemist.map((persona) => (
+    <li>
+      <img src={getimageUrl(persona)} alt={persona.name} />
+
+      <p>
+        <b>{persona.name}: </b>
+        {persona.profession} known for {persona.accomplishment}
+      </p>
+    </li>
+  ));
+
   return (
     <>
       <ul>{listaElementos}</ul>
+
+      <ul>{chemistListaElementos}</ul>
     </>
   );
 };
