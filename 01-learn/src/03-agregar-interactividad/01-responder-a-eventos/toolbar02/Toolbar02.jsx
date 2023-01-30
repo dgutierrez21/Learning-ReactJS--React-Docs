69b48360fbfaa895c584e365ee7dc85e43c6305d
Dixon G <dxxn0101@gmail.com>
2023-01-30 09:45:21 -0600

    Nombrar props de manejadores de eventos

--- 01-learn/src/03-agregar-interactividad/01-responder-a-eventos/toolbar02/Toolbar02.jsx
+++ 01-learn/src/03-agregar-interactividad/01-responder-a-eventos/toolbar02/Toolbar02.jsx
@@ -4,20 +4,25 @@ export const Toolbar02 = () => {
       <BotonPlay nombrePelicula="Rápidos y Furiosos 9" />
 
       <BotonSubir />
+
+      <ToolbarEJ02
+        onPlayMovie={() => alert("Reproduciendo película")}
+        onUploadImage={() => alert("Subiendo imagen")}
+      />
     </>
   );
 };
 
-function Boton({ onClick, children }) {
+function Boton({ onSmash, children }) {
   return (
-    <button className="btn btn-primary me-3" onClick={onClick}>
+    <button className="btn btn-primary me-3" onClick={onSmash}>
       {children}
     </button>
   );
 }
 
 function BotonSubir() {
-  return <Boton onClick={() => alert("Subiendo imagen")}>Subir Imagen</Boton>;
+  return <Boton onSmash={() => alert("Subiendo imagen")}>Subir Imagen</Boton>;
 }
 
 function BotonPlay({ nombrePelicula }) {
@@ -25,5 +30,15 @@ function BotonPlay({ nombrePelicula }) {
     alert(`Reproduciendo ${nombrePelicula}`);
   }
 
-  return <Boton onClick={handlePlayClick}>Reproducir "{nombrePelicula}"</Boton>;
+  return <Boton onSmash={handlePlayClick}>Reproducir "{nombrePelicula}"</Boton>;
 }
+
+export const ToolbarEJ02 = ({ onPlayMovie, onUploadImage }) => {
+  return (
+    <>
+      <Boton onSmash={onPlayMovie}>Reproducir película</Boton>
+
+      <Boton onSmash={onUploadImage}>Subir imagen</Boton>
+    </>
+  );
+};
