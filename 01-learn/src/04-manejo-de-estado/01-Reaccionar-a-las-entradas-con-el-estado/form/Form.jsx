3cbad11583d001727ed91cc5acbeefee8789279c
Dixon G <dxxn0101@gmail.com>
2023-02-03 21:02:38 -0600

    Paso 5: Conectar los controladores de eventos para establecer el estado

--- 01-learn/src/04-manejo-de-estado/01-Reaccionar-a-las-entradas-con-el-estado/form/Form.jsx
+++ 01-learn/src/04-manejo-de-estado/01-Reaccionar-a-las-entradas-con-el-estado/form/Form.jsx
@@ -27,10 +27,7 @@ export const Form = ({ estado = "vacio" }) => {
   );
 };
 
-const Form02 = (
-  // Prueba 'enviando', 'error', 'exito':
-  { estado = "vacio" }
-) => {
+const Form02 = () => {
   const [respuesta, setRespuesta] = useState("");
   const [error, setError] = useState(null);
 
@@ -38,6 +35,24 @@ const Form02 = (
 
   if (estado === "exito") {
     return <h1>¡Así es!</h1>;
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    setEstado("enviando");
+
+    try {
+      await enviarForm(respuesta);
+      setEstado("exito");
+    } catch (err) {
+      setEstado("escribiendo");
+      setError(err);
+    }
+  }
+
+  function handleCambiarTextArea(e) {
+    setRespuesta(e.target.value);
   }
 
   return (
@@ -49,28 +64,48 @@ const Form02 = (
         potable?
       </p>
 
-      <form action="">
+      <form onSubmit={handleSubmit} action="">
         <textarea
           name=""
           id=""
           cols="30"
           rows="10"
+          value={respuesta}
+          onChange={handleCambiarTextArea}
           disabled={estado === "enviando"}
         ></textarea>
         <br />
         <button
           className="btn btn-primary"
-          disabled={estado === "vacio" || estado === "enviando"}
+          disabled={respuesta.length === 0 || estado === "enviando"}
         >
           Enviar
         </button>
 
-        {estado === "error" && (
-          <p className="text-danger fw-bold h5 mt-2">
-            Buena suposición pero una respuesta incorrecta. Inténtalo de nuevo.
-          </p>
+        {error !== null && (
+          <p className="text-danger fw-bold h5 mt-2">{error.message}</p>
         )}
       </form>
     </>
   );
 };
+
+function enviarForm(respuesta) {
+  // Finge que está llegando a la red.
+
+  return new Promise((resolver, rechazar) => {
+    setTimeout(() => {
+      let debeError = respuesta.toLowerCase() !== "lima";
+
+      if (debeError) {
+        rechazar(
+          new Error(
+            "Buena suposición pero una respuesta incorrecta. Inténtalo de nuevo."
+          )
+        );
+      } else {
+        resolver();
+      }
+    }, 1500);
+  });
+}
