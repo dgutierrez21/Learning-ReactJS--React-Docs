0f7e9f0a96ec1f4df6a904902b5345cb36bfc370
Dixon G <dxxn0101@gmail.com>
2023-02-05 08:15:55 -0600

    Conservar y reiniciar el estado

---
+++ 01-learn/src/04-manejo-de-estado/04-Conservar-y-reiniciar-el-estado/ConservarYReiniciarElEstado.jsx
@@ -0,0 +1,6 @@
+
+export const ConservarYReiniciarElEstado = () => {
+  return (
+    <div>ConservarYReiniciarElEstado</div>
+  )
+}
--- 01-learn/src/04-manejo-de-estado/ManejoDeEstado.jsx
+++ 01-learn/src/04-manejo-de-estado/ManejoDeEstado.jsx
@@ -1,5 +1,6 @@
 import ElegirLaEstructuraDelEstado from "./02-Elegir-la-estructura-del-Estado/ElegirLaEstructuraDelEstado";
 import CompartirEstadosEntreComponentes from "./03-Compartir-estados-entre-componentes/CompartirEstadosEntreComponentes";
+import { ConservarYReiniciarElEstado } from "./04-Conservar-y-reiniciar-el-estado/ConservarYReiniciarElEstado";
 import ReaccionarALasEntradasConElEstado from "./07-Ampliación-con-Reducer-y-Context/ReaccionarALasEntradasConElEstado";
 
 const ManejoDeEstado = () => {
@@ -7,7 +8,8 @@ const ManejoDeEstado = () => {
     <>
       {/* <ReaccionarALasEntradasConElEstado /> */}
       {/* <ElegirLaEstructuraDelEstado/> */}
-      <CompartirEstadosEntreComponentes />
+      {/* <CompartirEstadosEntreComponentes /> */}
+      <ConservarYReiniciarElEstado />
     </>
   );
 };
