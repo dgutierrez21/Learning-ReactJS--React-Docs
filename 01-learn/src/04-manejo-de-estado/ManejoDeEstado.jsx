d16fd4510fbae8531146a3e5fd6c83ed4a773995
Dixon G <dxxn0101@gmail.com>
2023-02-07 08:10:36 -0600

    Extracción de lógica de estado en un reductor

--- 01-learn/src/04-manejo-de-estado/ManejoDeEstado.jsx
+++ 01-learn/src/04-manejo-de-estado/ManejoDeEstado.jsx
@@ -1,6 +1,7 @@
 import ElegirLaEstructuraDelEstado from "./02-Elegir-la-estructura-del-Estado/ElegirLaEstructuraDelEstado";
 import CompartirEstadosEntreComponentes from "./03-Compartir-estados-entre-componentes/CompartirEstadosEntreComponentes";
 import { ConservarYReiniciarElEstado } from "./04-Conservar-y-reiniciar-el-estado/ConservarYReiniciarElEstado";
+import { ExtraccionDeLaLogicaDeEstadosEnUnReductor } from "./05-Extracción-de-la-lógica-de-estados-en-un-reductor/ExtraccionDeLaLogicaDeEstadosEnUnReductor";
 import ReaccionarALasEntradasConElEstado from "./07-Ampliación-con-Reducer-y-Context/ReaccionarALasEntradasConElEstado";
 
 const ManejoDeEstado = () => {
@@ -9,7 +10,8 @@ const ManejoDeEstado = () => {
       {/* <ReaccionarALasEntradasConElEstado /> */}
       {/* <ElegirLaEstructuraDelEstado/> */}
       {/* <CompartirEstadosEntreComponentes /> */}
-      <ConservarYReiniciarElEstado />
+      {/* <ConservarYReiniciarElEstado /> */}
+      <ExtraccionDeLaLogicaDeEstadosEnUnReductor />
     </>
   );
 };
