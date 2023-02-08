bd235a6cee18df590fd1662f114501903fa581a4
Dixon G <dxxn0101@gmail.com>
2023-02-08 13:51:11 -0600

    Corregir componente duplicado

--- 01-learn/src/04-manejo-de-estado/ManejoDeEstado.jsx
+++ 01-learn/src/04-manejo-de-estado/ManejoDeEstado.jsx
@@ -1,19 +1,20 @@
+import ReaccionarALasEntradasConElEstado from "./01-Reaccionar-a-las-entradas-con-el-estado/ReaccionarALasEntradasConElEstado";
 import ElegirLaEstructuraDelEstado from "./02-Elegir-la-estructura-del-Estado/ElegirLaEstructuraDelEstado";
 import CompartirEstadosEntreComponentes from "./03-Compartir-estados-entre-componentes/CompartirEstadosEntreComponentes";
 import { ConservarYReiniciarElEstado } from "./04-Conservar-y-reiniciar-el-estado/ConservarYReiniciarElEstado";
 import { ExtraccionDeLaLogicaDeEstadosEnUnReductor } from "./05-Extracción-de-la-lógica-de-estados-en-un-reductor/ExtraccionDeLaLogicaDeEstadosEnUnReductor";
 import PasarDatosEnProfundidadConElContexto from "./06-Pasar-datos-en-profundidad-con-el-contexto/PasarDatosEnProfundidadConElContexto";
-import ReaccionarALasEntradasConElEstado from "./07-Ampliación-con-Reducer-y-Context/ReaccionarALasEntradasConElEstado";
+
 
 const ManejoDeEstado = () => {
   return (
     <>
-      {/* <ReaccionarALasEntradasConElEstado /> */}
+      <ReaccionarALasEntradasConElEstado />
       {/* <ElegirLaEstructuraDelEstado/> */}
       {/* <CompartirEstadosEntreComponentes /> */}
       {/* <ConservarYReiniciarElEstado /> */}
       {/* <ExtraccionDeLaLogicaDeEstadosEnUnReductor /> */}
-      <PasarDatosEnProfundidadConElContexto/>
+      {/* <PasarDatosEnProfundidadConElContexto/> */}
     </>
   );
 };
