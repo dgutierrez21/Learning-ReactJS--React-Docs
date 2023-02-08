d534988edd1b97da233c3cc73d344cb1a04b4e01
Dixon G <dxxn0101@gmail.com>
2023-02-07 21:05:17 -0600

    Paso 3: Proporcionar el contexto

--- 01-learn/src/04-manejo-de-estado/06-Pasar-datos-en-profundidad-con-el-contexto/pageApp/components/Section.jsx
+++ 01-learn/src/04-manejo-de-estado/06-Pasar-datos-en-profundidad-con-el-contexto/pageApp/components/Section.jsx
@@ -1,3 +1,9 @@
-export const Section = ({ children }) => {
-  return <section className="border border-2 p-3">{children}</section>;
+import { levelContext } from "../levelContext";
+
+export const Section = ({ nivel, children }) => {
+  return (
+    <section className="border border-2 p-3">
+      <levelContext.Provider value={nivel}>{children}</levelContext.Provider>
+    </section>
+  );
 };
