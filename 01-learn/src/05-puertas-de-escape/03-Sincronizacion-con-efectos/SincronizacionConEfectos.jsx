00614443bd95754582addab61bc743f2cd78d2c8
Dixon G <dxxn0101@gmail.com>
2023-02-10 13:18:23 -0600

    Sincronización con efectos

---
+++ 01-learn/src/05-puertas-de-escape/03-Sincronizacion-con-efectos/SincronizacionConEfectos.jsx
@@ -0,0 +1,5 @@
+export const SincronizacionConEfectos = () => {
+  return (
+    <div>SincronizacionConEfectos</div>
+  )
+}
--- 01-learn/src/05-puertas-de-escape/PuertasDeEscape.jsx
+++ 01-learn/src/05-puertas-de-escape/PuertasDeEscape.jsx
@@ -1,11 +1,13 @@
 import { ReferenciarValoresConRefs } from "./01-Referenciar-valores-con-refs copy 7/ReferenciarValoresConRefs";
 import ManipulacionDelDOMConRefs from "./02-Manipulacion-del-DOM-con-Refs/ManipulacionDelDOMConRefs";
+import { SincronizacionConEfectos } from "./03-Sincronizacion-con-efectos/SincronizacionConEfectos";
 
 const PuertasDeEscape = () => {
   return (
     <>
       {/* <ReferenciarValoresConRefs /> */}
-      <ManipulacionDelDOMConRefs />
+      {/* <ManipulacionDelDOMConRefs /> */}
+      <SincronizacionConEfectos />
     </>
   );
 };
