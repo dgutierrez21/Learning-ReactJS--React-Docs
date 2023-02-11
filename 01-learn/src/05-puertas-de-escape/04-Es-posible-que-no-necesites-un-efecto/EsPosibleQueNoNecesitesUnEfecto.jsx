43425b47bedbf71480c1c202fd292b639900d8ec
Dixon G <dxxn0101@gmail.com>
2023-02-11 10:26:51 -0600

    Es posible que no necesites un efecto

---
+++ 01-learn/src/05-puertas-de-escape/04-Es-posible-que-no-necesites-un-efecto/EsPosibleQueNoNecesitesUnEfecto.jsx
@@ -0,0 +1,7 @@
+const EsPosibleQueNoNecesitesUnEfecto = () => {
+  return (
+    <div>EsPosibleQueNoNecesitesUnEfecto</div>
+  )
+}
+
+export default EsPosibleQueNoNecesitesUnEfecto
--- 01-learn/src/05-puertas-de-escape/PuertasDeEscape.jsx
+++ 01-learn/src/05-puertas-de-escape/PuertasDeEscape.jsx
@@ -1,13 +1,15 @@
 import { ReferenciarValoresConRefs } from "./01-Referenciar-valores-con-refs copy 7/ReferenciarValoresConRefs";
 import ManipulacionDelDOMConRefs from "./02-Manipulacion-del-DOM-con-Refs/ManipulacionDelDOMConRefs";
 import { SincronizacionConEfectos } from "./03-Sincronizacion-con-efectos/SincronizacionConEfectos";
+import EsPosibleQueNoNecesitesUnEfecto from "./04-Es-posible-que-no-necesites-un-efecto/EsPosibleQueNoNecesitesUnEfecto";
 
 const PuertasDeEscape = () => {
   return (
     <>
       {/* <ReferenciarValoresConRefs /> */}
       {/* <ManipulacionDelDOMConRefs /> */}
-      <SincronizacionConEfectos />
+      {/* <SincronizacionConEfectos /> */}
+      <EsPosibleQueNoNecesitesUnEfecto />
     </>
   );
 };
