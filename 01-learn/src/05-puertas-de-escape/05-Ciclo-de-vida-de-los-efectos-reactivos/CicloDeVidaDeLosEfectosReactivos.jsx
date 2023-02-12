99760d9104db3599e7f6c43dfa8fc709ef3a8a5c
Dixon G <dxxn0101@gmail.com>
2023-02-12 10:08:55 -0600

    Ciclo de vida de los efectos reactivos

---
+++ 01-learn/src/05-puertas-de-escape/05-Ciclo-de-vida-de-los-efectos-reactivos/CicloDeVidaDeLosEfectosReactivos.jsx
@@ -0,0 +1,5 @@
+export const CicloDeVidaDeLosEfectosReactivos = () => {
+  return (
+    <div>CicloDeVidaDeLosEfectosReactivos</div>
+  )
+}
--- 01-learn/src/05-puertas-de-escape/PuertasDeEscape.jsx
+++ 01-learn/src/05-puertas-de-escape/PuertasDeEscape.jsx
@@ -2,6 +2,7 @@ import { ReferenciarValoresConRefs } from "./01-Referenciar-valores-con-refs cop
 import ManipulacionDelDOMConRefs from "./02-Manipulacion-del-DOM-con-Refs/ManipulacionDelDOMConRefs";
 import { SincronizacionConEfectos } from "./03-Sincronizacion-con-efectos/SincronizacionConEfectos";
 import EsPosibleQueNoNecesitesUnEfecto from "./04-Es-posible-que-no-necesites-un-efecto/EsPosibleQueNoNecesitesUnEfecto";
+import { CicloDeVidaDeLosEfectosReactivos } from "./05-Ciclo-de-vida-de-los-efectos-reactivos/CicloDeVidaDeLosEfectosReactivos";
 
 const PuertasDeEscape = () => {
   return (
@@ -9,7 +10,8 @@ const PuertasDeEscape = () => {
       {/* <ReferenciarValoresConRefs /> */}
       {/* <ManipulacionDelDOMConRefs /> */}
       {/* <SincronizacionConEfectos /> */}
-      <EsPosibleQueNoNecesitesUnEfecto />
+      {/* <EsPosibleQueNoNecesitesUnEfecto /> */}
+      <CicloDeVidaDeLosEfectosReactivos />
     </>
   );
 };
