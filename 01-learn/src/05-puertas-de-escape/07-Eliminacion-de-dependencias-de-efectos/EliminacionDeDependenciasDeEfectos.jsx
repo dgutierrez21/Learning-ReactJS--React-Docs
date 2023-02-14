25683224e3e9c70ff5f3758c71b49e9113d85495
Dixon G <dxxn0101@gmail.com>
2023-02-14 08:45:48 -0600

    Mueve objetos y funciones dinámicas dentro de tu Efecto

--- 01-learn/src/05-puertas-de-escape/07-Eliminacion-de-dependencias-de-efectos/EliminacionDeDependenciasDeEfectos.jsx
+++ 01-learn/src/05-puertas-de-escape/07-Eliminacion-de-dependencias-de-efectos/EliminacionDeDependenciasDeEfectos.jsx
@@ -1,8 +1,11 @@
 import { ChatApp } from "./chatApp/ChatApp";
+import { ChatApp02 } from "./chatApp02/ChatApp02";
 
 const EliminacionDeDependenciasDeEfectos = () => {
   return <>
   <ChatApp/>
+  <div className="separator"></div>
+  <ChatApp02/>
   </>;
 };
 
