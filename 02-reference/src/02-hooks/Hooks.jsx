43beb5cc8806e153b46a4ce257b8daef7c89b321
Dixon G <dxxn0101@gmail.com>
2023-02-22 08:56:30 -0600

    Ganchos de reacción incorporados

---
+++ 02-reference/src/02-hooks/Hooks.jsx
@@ -0,0 +1,11 @@
+import Overview from "./01-overview/Overview";
+
+const Hooks = () => {
+  return (
+    <>
+      <Overview />
+    </>
+  );
+};
+
+export default Hooks;
--- 02-reference/src/main.jsx
+++ 02-reference/src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Hooks from "./02-hooks/Hooks";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <Hooks />
+  </React.StrictMode>
 );
